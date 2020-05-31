24ed08e53c0a3f97c22ba7ca8bf2be2cbdc714dd
Frederik Schönfeldt <frederikschoenfeldt@gmail.com>
2020-05-31 20:49:24 +0200

    minor update - update gulpfile - update dev scripts - update scss - add alert to slider on missing variables - add soundplayer (WIP)

--- shortcodes.js
+++ shortcodes.js
@@ -1,26 +1,39 @@
 module.exports = {
-    slider: function (imgpath, pictures) {
+  slider: (imgpath, pictures) => {
+    console.log(imgpath);
+    console.log(pictures);
+    if (!imgpath || !pictures) {
       return `
-                <!-- Slider main container -->
-                <div class="swiper-container">
-                    <!-- Additional required wrapper -->
-                    <div class="swiper-wrapper">
-                        <!-- Slides -->
-                        ${createSwiperSlides(pictures, imgpath)}
-                    </div>
-                    <!-- If we need pagination -->
-                    <div class="swiper-pagination"></div>
+        <div class="alert alert-warning">
+            Warning: Missing imgpath/pictures!
+        </div>
+        `;
+    } else {
+      return `
+        <!-- Slider main container -->
+        <div class="swiper-container">
+            <!-- Additional required wrapper -->
+            <div class="swiper-wrapper">
+                <!-- Slides -->
+                ${createSwiperSlides(pictures, imgpath)}
+            </div>
+            <!-- If we need pagination -->
+            <div class="swiper-pagination"></div>
 
-                    <!-- If we need navigation buttons -->
-                    <div class="swiper-button-prev"></div>
-                    <div class="swiper-button-next"></div>
+            <!-- If we need navigation buttons -->
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
 
-                    <!-- If we need scrollbar
-                    <div class="swiper-scrollbar"></div> -->
-                </div>
+            <!-- If we need scrollbar
+            <div class="swiper-scrollbar"></div> -->
+        </div>
         `;
     }
-} 
+  },
+  audioplayer: (audiopath, filename) => {
+    
+  }
+}
 
 /**
  * Creates swiper slides image div based on input
@@ -29,11 +42,11 @@ module.exports = {
  * @returns {String} the html to render
  */
 const createSwiperSlides = (pictures, imgpath) => {
-    if (pictures) {
-        return (pictures.map(picture => {
-            return `<div class="swiper-slide swiper-slide--custom" style="background: url(/img/slider/${imgpath}/${picture})"></div>`;
-        })).join('');
-    } else {
-        return `<h1>Fehler: Es wurden keine Bilder definiert!</h1>`;
-    }
+  if (pictures) {
+    return (pictures.map(picture => {
+      return `<div class="swiper-slide swiper-slide--custom" style="background: url(/img/slider/${imgpath}/${picture})"></div>`;
+    })).join('');
+  } else {
+    return `<h1>Fehler: Es wurden keine Bilder definiert!</h1>`;
+  }
 }
